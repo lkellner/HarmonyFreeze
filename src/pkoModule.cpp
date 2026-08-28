6edfcabd53176b9188133c37a947b536cf71fa71
Laura Kellner <laura@roquetto.com>
2026-08-27 18:35:40 -0600

    static pivot1s are now being readjusted

--- src/pkoModule.cpp
+++ src/pkoModule.cpp
@@ -26,5 +26,44 @@ PkoModule::PkoModule(std::shared_ptr<FreezeManager> freezeManager,
 
 void PkoModule::readjustSecondary()
 {
+	FreezeManager* fm = getFreezeManagerPtr();
+
+	Math::Matrix4x4 oglChangeMatrix = fm->getFreezeMatrix();
+	Math::Matrix4x4 fieldsChangeMatrix = getFieldsModificationMatrix(getModulePtr()->sceneMetrics(), oglChangeMatrix);
+
+	Math::Point2d position;
+	m_pivot01Attr->getLocalValue(position);
+	Math::Point3d pos3d = Math::Point3d(position);
+
+	pos3d = fieldsChangeMatrix * pos3d;
+
+	std::shared_ptr<CO_OrCommand> curMacro = std::make_shared<CO_OrCommand>();
+
+	setStaticAttributes(pos3d, m_pivot01Attr, QLatin1String("pivot1"), *curMacro);
+
+	getFreezeManagerPtr()->addCommand(std::move(curMacro));
+}
+
+void PkoModule::setStaticAttributes(Math::Point3d position, AT_Position2dAttr* attr, QString attributeKeyword, CO_OrCommand& curMacro)
+{
+	clampValues(position);
+
+	//Similar to transformation module, can't set local value of combined paths
+
+	FreezeManager* fm = getFreezeManagerPtr();
+
+	if (fm->isExperimentalMode())
+	{
+		//C++
+		curMacro.add(Attr::Position2d::createSetLocalValueCmd(attr, position.x(), position.y()));
+	}
+	else
+	{
+		//JS
+
+		fm->applyAttributes(getModulePtr()->qualifiedName(),
+			StaticAttrData{ attributeKeyword + QLatin1String(".x"), position.x() },
+			StaticAttrData{ attributeKeyword + QLatin1String(".y"), position.y() });
+	}
 
 }
