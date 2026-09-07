7e2c4982eedf38727b7b2caf269faf5a91851126
Laura Kellner <laura@roquetto.com>
2026-09-07 16:23:42 -0600

    closing editor for freeform modules when experimental mode to avoid crahes

--- src/freeformModule.cpp
+++ src/freeformModule.cpp
@@ -17,6 +17,9 @@ FreeformModule::FreeformModule(std::shared_ptr<FreezeManager> freezeManager,
 		ModuleType moduleType)
 	: ModuleBase(std::move(freezeManager), modulePtr, moduleType)
 {
+	if (freezeManager->isExperimentalMode())
+		modulePtr->closeEditor();
+
 	AT_AttrList attrList = getAttributeList();
 
 	for (auto& attr : attrList)
